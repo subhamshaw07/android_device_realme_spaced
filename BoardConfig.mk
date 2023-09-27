realme-mt6781-dev/android_device_realme_spaced_old

spaced: Mention absolute path for clang
Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
Change-Id: I48357a044c4edb01202899e5fa6173d8f5b8b723
Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

BoardConfig.mk
@@ -67,6 +67,7 @@ TARGET_KERNEL_SOURCE := kernel/realme/mt6781
 TARGET_KERNEL_CONFIG := spaced_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := azure-clang
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/azure-clang
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_KERNEL_SEPARATED_DTBO := true

subhamshaw07/android_device_realme_spaced

BoardConfig.mk
@@ -67,6 +67,7 @@ TARGET_KERNEL_SOURCE := kernel/realme/mt6781
 TARGET_KERNEL_CONFIG := spaced_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := r487747
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r487747
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_KERNEL_IMAGE_NAME := Image.gz